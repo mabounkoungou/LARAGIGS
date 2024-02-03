af435f9501f433d69ce0d8f0e017b9d4b8664f3f
RAAD <mabounkoungou001@st.ug.edu.gh>
2024-02-03 19:36:59 +0000

    First Commit on Github

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    //Want to return the login and signup views
+
+    public function registration(){
+        return view("");
+    }
+    public function login(){
+        return view("");
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+Route::get("/",[HomeController::class,"index"])->name('home');
+Route::get("/account/login.php",[AccountController::class,"login"])->name('login');
+Route::get("/account/registration.php",[AccountController::class,"registration"])->name('registration');
+
