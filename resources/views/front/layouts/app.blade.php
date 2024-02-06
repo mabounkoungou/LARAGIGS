b150341fa08d85125dea58b84339b19b67764668
RAAD <mabounkoungou001@st.ug.edu.gh>
2024-02-06 10:42:39 +0000

    update

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -73,14 +73,14 @@ body {
 <header>
 	<nav class="navbar navbar-expand-lg navbar-light bg-white shadow py-3">
 		<div class="container">
-			<a class="navbar-brand" href="route{{('home')}}">LARA-GIGS</a>
+			<a class="navbar-brand" href="#">LARA-GIGS</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ms-0 ms-sm-0 me-auto mb-2 mb-lg-0 ms-lg-4">
 					<li class="nav-item">
-						<a class="nav-link" aria-current="page" href="{{route('account.authenticate')}}">Home</a>
+						<a class="nav-link" aria-current="page" href="{{route('home')}}">Home</a>
 					</li>	
 					<li class="nav-item">
 						<a class="nav-link" aria-current="page" href="jobs.html">Find Jobs</a>
