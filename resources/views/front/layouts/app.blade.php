dc5ab238b75a0169d10db8cd2615344bfa1fff0d
RAAD <mabounkoungou001@st.ug.edu.gh>
2024-02-07 11:09:21 +0000

    update

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -73,7 +73,7 @@ body {
 <header>
 	<nav class="navbar navbar-expand-lg navbar-light bg-white shadow py-3">
 		<div class="container">
-			<a class="navbar-brand" href="#">LARA-GIGS</a>
+			<a class="navbar-brand" href="{{route('home')}}">LARA-GIGS</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
